94e796a3d177425b26284805a85de2db5ba68862
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-05-09 20:53:13 +0200

    pierwszy działający przykład

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,10 +1,16 @@
 package edu.iis.mto.testreactor.atm;
 
+import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
+import edu.iis.mto.testreactor.atm.bank.AuthorizationToken;
 import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 class ATMachineTest {
 
@@ -17,11 +23,18 @@ class ATMachineTest {
     void setUp() {
         bank = Mockito.mock(Bank.class);
         ATM = new ATMachine(bank, Money.DEFAULT_CURRENCY);
+        List<BanknotesPack> list = new ArrayList<>();
+        list.add(BanknotesPack.create(10, Banknote.PL_10));
+        MoneyDeposit moneyDeposit = MoneyDeposit.create(Money.DEFAULT_CURRENCY, list);
+        ATM.setDeposit(moneyDeposit);
     }
 
     @Test
-    void itCompiles() {
-
+    void withdrawSomeCashTest() throws ATMOperationException, AuthorizationException {
+        PinCode pinCode = PinCode.createPIN(1, 2, 3, 4);
+        Card card = Card.create("123");
+        Mockito.when(bank.authorize(pinCode.getPIN(), card.getNumber())).thenReturn(AuthorizationToken.create("12345"));
+        ATM.withdraw(pinCode, card, new Money(new BigDecimal(10), Money.DEFAULT_CURRENCY));
     }
 
 }
